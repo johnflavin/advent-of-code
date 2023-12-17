260c882eecba5544d0c1b3330f33f63ddad3873b
John Flavin <flavinj@gmail.com>
2023-12-16 22:25:51 -0600

    Refactor 2021-12-15, speed it up 10-20%

--- aoc2021/day_15.py
+++ aoc2021/day_15.py
@@ -37,41 +37,34 @@ PART_ONE_RESULT = 626
 PART_TWO_RESULT = 2966
 
 
-def solution(weights: list[list[int]]) -> int:
+def solution(weights: tuple[tuple[int, ...], ...]) -> int:
     num_rows = len(weights)
     num_cols = len(weights[0])
 
     graph = networkx.DiGraph()
 
-    for row in range(num_rows):
-        for col in range(num_cols):
-            pt = (row, col)
-
-            # Add an edge going from each neighbor into this point,
-            #  weighted by this point's cost
-            graph.add_edges_from(
-                (neighbor, pt, {"weight": weights[row][col]})
-                for neighbor in neighbor_indices(pt, num_rows, num_cols)
-            )
-    # Find the shortest (least weighted) path through the graph
-    shortest_path = networkx.shortest_path(
-        graph, (0, 0), (num_rows - 1, num_cols - 1), "weight"
+    # Add an edge going from each neighbor into this point,
+    #  weighted by this point's cost
+    graph.add_edges_from(
+        (neighbor, (row_idx, col_idx), {"weight": weight})
+        for row_idx, row in enumerate(weights)
+        for col_idx, weight in enumerate(row)
+        for neighbor in neighbor_indices((row_idx, col_idx), num_rows, num_cols)
     )
 
-    # Sum up the weights from all edges traversed on the path
-    return (
-        sum(weights[path_row][path_col] for path_row, path_col in shortest_path)
-        - weights[0][0]
+    # Find the shortest (least weighted) path through the graph
+    return networkx.shortest_path_length(
+        graph, (0, 0), (num_rows - 1, num_cols - 1), "weight"
     )
 
 
 def part_one(lines: Iterable[str]) -> int:
-    weights = [[int(x) for x in line] for line in lines]
+    weights = tuple(tuple(map(int, line)) for line in lines)
     return solution(weights)
 
 
 def part_two(lines: Iterable[str]) -> int:
-    base_weights = [[int(x) for x in line] for line in lines]
+    base_weights = tuple(tuple(map(int, line)) for line in lines)
     base_num_rows = len(base_weights)
     base_num_cols = len(base_weights[0])
     num_rows = base_num_rows * 5
@@ -81,9 +74,9 @@ def part_two(lines: Iterable[str]) -> int:
         for col in range(num_cols):
             row_tile, base_row = divmod(row, base_num_rows)
             col_tile, base_col = divmod(col, base_num_cols)
-            weight = base_weights[base_row][base_col] + row_tile + col_tile
-            if weight > 9:
-                weight -= 9
-            weights[row][col] = weight
+            weights[row][col] = (
+                base_weights[base_row][base_col] + row_tile + col_tile
+            ) % 9 or 9
+    weights = tuple(tuple(line) for line in weights)
 
     return solution(weights)
