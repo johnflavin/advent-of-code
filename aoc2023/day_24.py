2805f2309a59ef03904914bdd0e7f938b7848fcc
John Flavin <flavinj@gmail.com>
2024-01-04 15:43:40 -0600

    Day 24 part 2

--- aoc2023/day_24.py
+++ aoc2023/day_24.py
@@ -11,11 +11,91 @@ Find how many pairs' paths will intersect in the future within the test area.
 we only count X once for the earliest intersection.)
 
 PART 2
+At what initial position and velocity could you throw a rock that would hit
+all the hailstones?
+
+Sum the x, y, and z of the initial position.
+
+SOLUTION
+This took me a long time to solve. I finally cracked it on Jan. 4, 2024.
+
+The idea is this. For every hailstone, we will have a (vector) equation:
+P + ti*V = Pi + ti*Vi
+
+Where P and V are the unknown rock position and velocity,
+P = (px, py, pz); V = (vx, vy, vz)
+Pi and Vi are the known position and velocity of hailstone i,
+Pi = (pix, piy, piz); Vi = (vix, viy, viz)
+and ti is the time it takes for them to intersect.
+
+From this we can immediately see that the vector components all must be equal.
+That gives us equations for ti and some of the components:
+ti = -(px-pix)/(vx-vix) = -(py-piy)/(vy-viy) = -(pz-piz)/(vz-viz)
+
+With a single hailstone, we get two unique equations for the p and v components.
+The third is redundant. But that lets us eliminate two of the components,
+say vx and vy, in terms of vz and the ps.
+
+We will get the rest of the information by considering more than one hailstone.
+We connect an arbitrary pair i,j.
+Say we start from
+
+    (vx-vix)/(px-pix) = (vz-viz)/(pz-piz)
+    (vy-viy)/(py-piy) = (vz-viz)/(pz-piz)
+    (vy-vjy)/(py-pjy) = (vx-vjx)/(px-pjx)
+
+By substituting the first two into the third we can show that
+
+(1) (vz-viz)/(pz-piz) =
+        [(viy-vjy)(px-pjx)+(vjx-vix)(py-pjy)]/[(px-pix)(py-pjy)-(py-piy)(px-pjx)]
+
+This is symmetric under exchange of x and y. The numerator and denominator
+both pick up a negative, which cancel.
+But it is not symmetric under exchange of i and j. If we exchange those indices, we get
+
+(2) (vz-vjz)/(pz-pjz) =
+        [(viy-vjy)(px-pix)+(vjx-vix)(py-piy)]/[(px-pix)(py-pjy)-(py-piy)(px-pjx)]
+
+which is not the same RHS as (1). They have the same denominator
+but different numerators. However, these two must be the same.
+i and j are arbitrary labels; so long as they aren't equal they can be anything,
+so choosing i first then j must be equivalent to choosing j first then i.
+
+What we'll do is subtract the two equations: (1) - (2).
+After expanding everything out and grouping terms (which I won't show)
+there are nice cancellations. All the vz terms go away, as do terms
+quadratic in the p components.
+What we are left with can be written as
+
+(3) (Vi-Vj)×(Pi-Pj)⋅P = (Vi-Vj)⋅Pi×Pj
+
+Everything in this equation is known except for P,
+meaning this is what we need to solve.
+P has three unknown components, so we need three equations.
+We get those by selecting any three hailstones 0, 1, and 2
+and grouping them into three pairs i=0, j=1; i=0, j=2; and i=1, j=2.
+Each one of those i,j pairs gives us another equation from (3).
+We organize those as rows of a single matrix equation
+
+    C P = D
+
+Where the rows of matrix C are the (Vi-Vj)×(Pi-Pj) vectors, and
+the components of vector D are the (Vi-Vj)⋅Pi×Pj scalars.
+
+The answer we are looking for is
+
+    P = C^-1 D
+
+In the code I've written out that matrix inverse by hand.
+Yes, I'm aware I could use numpy. But I did the math by hand,
+so why not just finish the job?
 """
 
+import logging
 from .util import Coord
 from collections.abc import Iterable
 from itertools import combinations
+from math import sumprod
 
 
 PART_ONE_EXAMPLE = """\
@@ -29,11 +109,12 @@ PART_ONE_EXAMPLE_TEST_AREA = [7, 27]
 PART_ONE_EXAMPLE_RESULT = 2
 PART_ONE_TEST_AREA = [200000000000000, 400000000000000]
 PART_ONE_RESULT = 21785
-PART_TWO_EXAMPLE = """\
-"""
-PART_TWO_EXAMPLE_RESULT = None
-PART_TWO_RESULT = None
+PART_TWO_EXAMPLE = PART_ONE_EXAMPLE
+PART_TWO_EXAMPLE_RESULT = 47
+PART_TWO_RESULT = 554668916217145
 
+
+log = logging.getLogger(__name__)
 
 type Vector = tuple[int, int, int]
 
@@ -118,6 +199,100 @@ def part_one(lines: Iterable[str]) -> int:
     return total
 
 
+def intial_position(
+    p0: Vector, v0: Vector, p1: Vector, v1: Vector, p2: Vector, v2: Vector
+) -> Vector:
+    def delta(vec_i, vec_j):
+        return tuple(va - vb for va, vb in zip(vec_i, vec_j))
+
+    def cross(vect_i, vec_j):
+        return (
+            vect_i[1] * vec_j[2] - vect_i[2] * vec_j[1],
+            vect_i[2] * vec_j[0] - vect_i[0] * vec_j[2],
+            vect_i[0] * vec_j[1] - vect_i[1] * vec_j[0],
+        )
+
+    log.debug("p0 v0 = %s %s", p0, v0)
+    log.debug("p1 v1 = %s %s", p1, v1)
+    log.debug("p2 v2 = %s %s", p2, v2)
+
+    dv01 = delta(v0, v1)
+    dv02 = delta(v0, v2)
+    dv12 = delta(v1, v2)
+    c01 = cross(dv01, delta(p0, p1))
+    c02 = cross(dv02, delta(p0, p2))
+    c12 = cross(dv12, delta(p1, p2))
+    dvij_dot_pi_cross_pj = (
+        sumprod(dv01, cross(p0, p1)),
+        sumprod(dv02, cross(p0, p2)),
+        sumprod(dv12, cross(p1, p2)),
+    )
+    if log.isEnabledFor(logging.DEBUG):
+        log.debug("p0-p1 = %s", delta(p0, p1))
+        log.debug("p0-p2 = %s", delta(p0, p2))
+        log.debug("p1-p2 = %s", delta(p1, p2))
+        log.debug("v0-v1 = %s", dv01)
+        log.debug("v0-v2 = %s", dv02)
+        log.debug("v1-v2 = %s", dv12)
+        log.debug("c01 = (v0-v1)×(p0-p1) = %s", c01)
+        log.debug("c02 = (v0-v2)×(p0-p2) = %s", c02)
+        log.debug("c12 = (v1-v2)×(p1-p2) = %s", c12)
+        log.debug("(v0-v1)⋅p0×p1 = %s", dvij_dot_pi_cross_pj[0])
+        log.debug("(v0-v2)⋅p0×p2 = %s", dvij_dot_pi_cross_pj[1])
+        log.debug("(v1-v2)⋅p1×p2 = %s", dvij_dot_pi_cross_pj[2])
+
+    c_inverse_unnorm = (
+        (
+            c12[1] * c02[2] - c12[2] * c02[1],
+            c01[1] * c12[2] - c01[2] * c12[1],
+            c01[2] * c02[1] - c01[1] * c02[2],
+        ),
+        (
+            c12[2] * c02[0] - c12[0] * c02[2],
+            c01[2] * c12[0] - c01[0] * c12[2],
+            c01[0] * c02[2] - c01[2] * c02[0],
+        ),
+        (
+            c12[0] * c02[1] - c12[1] * c02[0],
+            c01[0] * c12[1] - c01[1] * c12[0],
+            c01[1] * c02[0] - c01[0] * c02[1],
+        ),
+    )
+    log.debug("c inverse unnorm = %s", c_inverse_unnorm)
+    determinant = sum(c01[i] * c_inverse_unnorm[i][0] for i in range(3))
+    log.debug("|C| = %s", determinant)
+
+    p: Vector = tuple(
+        sumprod(c_inverse_row, dvij_dot_pi_cross_pj) // determinant
+        for c_inverse_row in c_inverse_unnorm
+    )
+
+    if log.isEnabledFor(logging.DEBUG):
+        log.debug("p = %s", p)
+        log.debug("Sanity check")
+        lhs = (
+            sumprod(c01, p),
+            sumprod(c02, p),
+            sumprod(c12, p),
+        )
+        ij = ((0, 1), (0, 2), (1, 2))
+        for left, right, (i, j) in zip(lhs, dvij_dot_pi_cross_pj, ij):
+            eq_str = "=" if left == right else "!"
+            log.debug(
+                "(v%d-v%d)×(p%d-p%d)⋅p = %s %s= %s", i, j, i, j, left, eq_str, right
+            )
+
+    return p
+
+
 def part_two(lines: Iterable[str]) -> int:
-    # thing = (line for line in lines if line)
-    return -1
+    position_velocity_pairs: list[tuple[Vector, Vector]] = []
+    for line in lines:
+        position_velocity_pairs.append(parse(line))
+
+    p = intial_position(
+        *position_velocity_pairs[0],
+        *position_velocity_pairs[1],
+        *position_velocity_pairs[2],
+    )
+    return sum(p)
