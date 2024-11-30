9f7ef91d9432a0d57d4305fd23a0ac4ff8308906
John Flavin <flavinj@gmail.com>
2024-11-29 22:13:29 -0600

    Add 2023 day 25 solution

--- src/aoc/aoc2023/day_25.py
+++ src/aoc/aoc2023/day_25.py
@@ -9,9 +9,9 @@ Multiply the sizes of the groups together.
 
 PART 2
 """
+from collections.abc import Iterable
 
 import networkx as nx
-from collections.abc import Iterable
 
 
 PART_ONE_EXAMPLE = """\
@@ -30,11 +30,11 @@ rzs: qnr cmg lsr rsh
 frs: qnr lhk lsr
 """
 PART_ONE_EXAMPLE_RESULT = 54
-PART_ONE_RESULT = None
+PART_ONE_RESULT = 543256
 PART_TWO_EXAMPLE = """\
 """
 PART_TWO_EXAMPLE_RESULT = None
-PART_TWO_RESULT = None
+PART_TWO_RESULT = -1
 
 
 def parse_lines_to_graph(lines: Iterable[str]) -> nx.Graph:
@@ -50,8 +50,14 @@ def parse_lines_to_graph(lines: Iterable[str]) -> nx.Graph:
 
 def part_one(lines: Iterable[str]) -> int:
     # thing = (line for line in lines if line)
-    g = parse_lines_to_graph(lines)
-    return len(g)
+    G = parse_lines_to_graph(lines)
+
+    bisection = nx.spectral_bisection(G)
+
+    if len(bisection) != 2:
+        raise RuntimeError("Did not find correct cut")
+
+    return len(bisection[0]) * len(bisection[1])
 
 
 def part_two(lines: Iterable[str]) -> int:
