846d6bb481922a1ac545ce6f60fbb0752fe0d4e8
John Flavin <flavinj@gmail.com>
2024-12-02 12:55:36 -0600

    2024-12-02: add part 2 answer + mild refactors

--- src/aoc/aoc2024/day_02.py
+++ src/aoc/aoc2024/day_02.py
@@ -25,7 +25,7 @@ PART_ONE_EXAMPLE_RESULT = 2
 PART_ONE_RESULT = 510
 PART_TWO_EXAMPLE = PART_ONE_EXAMPLE
 PART_TWO_EXAMPLE_RESULT = 4
-PART_TWO_RESULT = None
+PART_TWO_RESULT = 553
 
 
 log = logging.getLogger(__name__)
@@ -35,32 +35,31 @@ def parse(line: str) -> tuple[int, ...]:
     return tuple(map(int, line.split()))
 
 
-def take_diffs(line: Iterable[int]) -> tuple[int, ...]:
-    return tuple(b - a for a, b in zip(line, line[1:]))
-
-
-def in_range(test: int, low: int, high: int) -> bool:
-    return low <= test <= high
-
-
-def is_safe(diffs: tuple[int, ...]) -> bool:
+def is_safe(line: Iterable[int]) -> bool:
+    line = tuple(line)
+    diffs = tuple(b - a for a, b in zip(line, line[1:]))
     sign = 1 if diffs[0] > 0 else -1
     for d in diffs:
         d_pos = d * sign
-        if d_pos < 0 or not in_range(d_pos, 1, 3):
+        if d_pos < 1 or d_pos > 3:
             return False
     return True
 
 
+def is_safe_part_1(line: str) -> bool:
+    return is_safe(parse(line))
+
+
+def is_safe_part_2(line: str) -> bool:
+    return any(
+        is_safe(el for idx, el in enumerate(parse(line)) if idx != skip_idx)
+        for skip_idx in range(len(line))
+    )
+
+
 def part_one(lines: Iterable[str]) -> int:
-    return sum(is_safe(take_diffs(parse(line))) for line in lines)
+    return sum(map(is_safe_part_1, lines))
 
 
 def part_two(lines: Iterable[str]) -> int:
-    return sum(
-        any(
-            is_safe(take_diffs([el for idx, el in enumerate(parse(line)) if idx != i]))
-            for i in range(len(line))
-        )
-        for line in lines
-    )
+    return sum(map(is_safe_part_2, lines))
