8275598a4305b2c3349c691fddf8d9e7cbd33f21
John Flavin <flavinj@gmail.com>
2024-12-23 19:31:09 -0600

    Refactor 2023-12-23. Simplify implementation, acheive moderate speedup. Still slow, though.

--- src/aoc/aoc2023/day_23.py
+++ src/aoc/aoc2023/day_23.py
@@ -10,11 +10,10 @@ PART 2
 Ignore slopes
 """
 import logging
-from collections import deque
+from collections import defaultdict
 from collections.abc import Iterable
-from functools import cache
 
-from aoc.util import Coord, OFFSETS
+from aoc.util import Coord, neighbors, add
 
 
 PART_ONE_EXAMPLE = """\
@@ -52,87 +51,169 @@ PART_TWO_RESULT = 6670
 log = logging.getLogger(__name__)
 is_debug = log.isEnabledFor(logging.DEBUG)
 
+type Slopes = dict[Coord, Coord]
+
 SLOPES = ("^", "<", "v", ">")
-OFFSETS_BY_SLOPE = dict(zip(SLOPES, OFFSETS))
+OFFSETS_BY_SLOPE = {"^": (0, -1), "<": (-1, 0), "v": (0, 1), ">": (1, 0)}
+NO_SLOPE = (0, 0)
 
 
-def find_path_edges(
-    lines: tuple[str, ...], ignore_slopes: bool = False
-) -> tuple[tuple[int, tuple[Coord, ...]], ...]:
-    rows = len(lines)
-    cols = len(lines[0])
-    start = (0, lines[0].find("."))
-    end = (rows - 1, lines[-1].find("."))
+def parse_lines(lines: Iterable[str]) -> tuple[Coord, Coord, set[Coord], Slopes]:
+    walls = set()
+    slopes = {}
+    for y, line in enumerate(lines):
+        for x, ch in enumerate(line):
+            if ch == "#":
+                walls.add((x, y))
+            elif ch in SLOPES:
+                slopes[(x, y)] = OFFSETS_BY_SLOPE[ch]
+            elif y == 0 and ch == ".":
+                start = (x, y)
+                walls.add((x, y - 1))
+    # Go through last line again to find end
+    for x, ch in enumerate(line):
+        if ch == ".":
+            end = (x, y)
+            walls.add((x, y + 1))
+    return start, end, walls, slopes
 
-    @cache
-    def find_path_segment(
-        pt: Coord, prev_point: Coord | None = None
-    ) -> tuple[Coord, int, tuple[Coord, ...]]:
-        seen = set()
-        if prev_point is not None:
-            seen.add(prev_point)
-        path_len = 1
-        while True:
-            if is_debug:
-                log.debug(f" + {pt}")
-            seen.add(pt)
-            current_symbol = lines[pt[0]][pt[1]]
-            offsets = (
-                OFFSETS
-                if ignore_slopes or current_symbol not in SLOPES
-                else (OFFSETS_BY_SLOPE[current_symbol],)
-            )
-            next_steps: list[Coord] = [
-                (row, col)
-                for offset in offsets
-                if (
-                    -1 < (row := pt[0] + offset[0]) < rows
-                    and -1 < (col := pt[1] + offset[1]) < cols
-                    and lines[row][col] != "#"
-                    and (row, col) not in seen
+
+def walk(start: Coord, end: Coord, walls: set[Coord], slopes: Slopes) -> int:
+    """
+    Here's what we want to do.
+    Build a small graph of start, end, and "branch points":
+    points with more than two non-wall neighbors.
+    Once we build that we do a simple walk to find paths from start to end.
+
+    Building that smaller graph is the main task.
+    We have an outer queue over branch points (initialized to "start").
+    When we pop a branch point off the queue, we initialize an inner queue
+    to the neighbors of the branch point.
+    We walk until we get to another branch point or hit a slope we can't climb.
+    We store a map of maps. The outer key is the branch point. The inner key
+    is the branch point neighbor, the value is a tuple of the ending branch point
+    and the length of the path between neighbor and end point (inclusive), or
+    if we hit a wall the value should be None, -1.
+
+    So that's three layers of loop:
+    1. Branch points
+    2. First neighbors
+    3. The segment between branch points
+    """
+
+    graph = defaultdict(dict)
+
+    visited_branches = set()
+    branch_pt_queue = [start]
+    while branch_pt_queue:
+
+        # This is a branch point.
+        # Try walking down each of its neighbors.
+        bp = branch_pt_queue.pop()
+        bp_neighbors = [
+            n
+            for n in neighbors(bp)
+            if (
+                n not in walls  # No walls
+                and n not in visited_branches  # No redos
+                and (
+                    (bpslope := slopes.get(bp, NO_SLOPE)) == NO_SLOPE
+                    or n == add(bp, bpslope)
                 )
-            ]
-            if len(next_steps) == 1:
-                # Just continue stepping forward
-                pt = next_steps[0]
-                path_len += 1
-            else:
-                if is_debug:
-                    if not next_steps:
-                        log.debug(" ++ End of path. No next steps.")
+                and (
+                    (nslope := slopes.get(n, NO_SLOPE)) == NO_SLOPE
+                    or bp != add(n, nslope)
+                )
+            )
+        ]
+
+        # Traverse each branch
+        while bp_neighbors:
+            bpn = bp_neighbors.pop()
+            if bpn in visited_branches:
+                continue
+            visited_branches.add(bpn)
+            log.debug("Branch %s->%s", bp, bpn)
+
+            seg_visited = {bp}
+            seg_queue = [bpn]
+            while seg_queue:
+                pt = seg_queue.pop()
+
+                if pt in seg_visited:
+                    continue
+                seg_visited.add(pt)
+                log.debug(" + %s", pt)
+
+                ptns = [
+                    n
+                    for n in neighbors(pt)
+                    if (
+                        n not in walls  # No walls
+                        and n not in seg_visited  # No backtracking
+                    )
+                ]
+                num_n = len(ptns)
+                if num_n > 1 or num_n == 0 and pt == end:
+                    # We've reached a branch point
+
+                    # End this segment
+                    lsv = len(seg_visited) - 1
+                    log.debug("Recording %s->%s->%s %d", bp, bpn, pt, lsv)
+                    graph[bp][bpn] = (pt, lsv)
+
+                    if pt != end:
+                        # Queue the branch point
+                        branch_pt_queue.append(pt)
+                else:
+                    # This is not a branch point
+                    traversable = [
+                        n
+                        for n in ptns
+                        if (
+                            (
+                                (pt_slope := slopes.get(pt, NO_SLOPE)) == NO_SLOPE
+                                or n == add(pt, pt_slope)
+                            )
+                            and (
+                                (n_slope := slopes.get(n, NO_SLOPE)) == NO_SLOPE
+                                or pt != add(n, n_slope)
+                            )
+                        )
+                    ]
+                    if len(traversable) == 1:
+                        # No choice at all. Keep walking.
+                        seg_queue.append(traversable[0])
                     else:
-                        log.debug(f" ++ Choices: {next_steps}")
-                return pt, path_len, tuple(next_steps)
+                        log.debug("Dead end", bp, bpn, pt)
+            log.debug("Branch %s->%s finished", bp, bpn)
+        log.debug("Branch pt %s finished", bp)
 
-    find_path_segment.cache_clear()
+    # We have massively simplified the graph down to only the branch points.
+    # Now construct all the valid paths from the segments
+    log.debug("Finding path lens")
     paths = []
-    frontier: deque[tuple[int, tuple[Coord, ...], Coord]] = deque(
-        [(0, (start,), start)]
-    )
-    while frontier:
-        path_len, path, next_step = frontier.popleft()
-        prev_step = path[-1] if path[-1] != next_step else None
-        if is_debug:
-            log.debug(f"find_path_segment({next_step}, {prev_step})")
-        next_step_end_node, next_step_len, next_next_steps = find_path_segment(
-            next_step, prev_step
-        )
-        if next_step_end_node in path:
+    queue = [(0, start, frozenset())]
+    while queue:
+        path_len, seg_start, visited = queue.pop()
+        if seg_start in visited:
             continue
-        path_len += next_step_len
-        path = (*path, next_step_end_node)
-        if next_step_end_node == end:
-            paths.append((path_len, path))
-            continue
-        frontier.extend((path_len, path, step) for step in next_next_steps)
+        visited = visited | {seg_start}
+        for _, (seg_end, seg_len) in graph[seg_start].items():
+            new_len = path_len + seg_len
+            if seg_end == end:
+                log.debug("Found end. Path len %d - %s", new_len, visited)
+                paths.append(new_len)
+            else:
+                queue.append((new_len, seg_end, visited))
 
-    return tuple(paths)
+    return max(paths)
 
 
 def find_max_path_len(lines: Iterable[str], ignore_slopes=False) -> int:
-    lines = tuple(lines)
-    paths = find_path_edges(lines, ignore_slopes)
-    return max(path_len for path_len, _ in paths) - 1
+    start, end, walls, slopes = parse_lines(lines)
+    slopes = {} if ignore_slopes else slopes
+    return walk(start, end, walls, slopes)
 
 
 def part_one(lines: Iterable[str]) -> int:
@@ -140,4 +221,4 @@ def part_one(lines: Iterable[str]) -> int:
 
 
 def part_two(lines: Iterable[str]) -> int:
-    return find_max_path_len(tuple(lines), ignore_slopes=True)
+    return find_max_path_len(lines, ignore_slopes=True)
