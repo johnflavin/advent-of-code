f468fa4c7a1b7e8bec5140d136ba4b801f744d91
John Flavin <flavinj@gmail.com>
2025-01-09 14:53:37 -0600

    Add 2019-12-05

---
+++ src/aoc/aoc2019/day_05.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""
+PART 1
+Add opcodes 3 and 4 to store input and produce output, respectively.
+Also add parameter modes.
+Provide 1 as input.
+Outputs should be all 0 except for last value, which is the answer.
+
+PART 2
+Implement opcodes 5, 6, 7, and 8
+Provide 5 as an input and return the only output.
+"""
+from collections.abc import Iterable
+
+from aoc.aoc2019.intcode import Intcode
+
+
+PART_ONE_EXAMPLE = """\
+"""
+PART_ONE_EXAMPLE_RESULT = None
+PART_ONE_RESULT = 6745903
+PART_TWO_EXAMPLE = """\
+"""
+PART_TWO_EXAMPLE_RESULT = None
+PART_TWO_RESULT = 9168267
+
+
+def part_one(lines: Iterable[str]) -> int:
+    program = [int(i) for i in "".join(lines).split(",")]
+    ic = Intcode(program)
+    outputs = ic.run(1)
+    if any(o != 0 for o in outputs[:-1]):
+        print("outputs should be 0:", outputs)
+    return outputs[-1]
+
+
+def part_two(lines: Iterable[str]) -> int:
+    program = [int(i) for i in "".join(lines).split(",")]
+    ic = Intcode(program)
+    outputs = ic.run(5)
+    return outputs[0]
--- tests/test_2019.py
+++ tests/test_2019.py
@@ -4,4 +4,4 @@ from aoc.testing.util import generate_pytest_generate_tests
 
 marks = {}
 
-pytest_generate_tests = generate_pytest_generate_tests(marks, 4)
+pytest_generate_tests = generate_pytest_generate_tests(marks, 5)
