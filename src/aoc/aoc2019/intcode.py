8e5860d0d7ff908e51392801f796906cd524aca1
John Flavin <flavinj@gmail.com>
2025-01-09 14:51:14 -0600

    Add opcodes 3, 4, 5, 6, 7, and 8, and tests for all of those

--- src/aoc/aoc2019/intcode.py
+++ src/aoc/aoc2019/intcode.py
@@ -15,6 +15,7 @@ class Intcode:
 
             param_pointer = self.pointer + 1
             if opcode == 1:
+                """Add"""
                 p1, p2, p3 = self.memory[param_pointer : param_pointer + 3]
 
                 param_modes, p1_mode = divmod(param_modes, 10)
@@ -29,6 +30,7 @@ class Intcode:
 
                 self.pointer += 4
             elif opcode == 2:
+                """Multiply"""
                 p1, p2, p3 = self.memory[param_pointer : param_pointer + 3]
 
                 param_modes, p1_mode = divmod(param_modes, 10)
@@ -40,6 +42,72 @@ class Intcode:
 
                 assert p3_mode == 0
                 self.memory[p3] = result
+
+                self.pointer += 4
+            elif opcode == 3:
+                """Store input"""
+                p1 = self.memory[param_pointer]
+                self.memory[p1] = input_val
+                self.pointer += 2
+            elif opcode == 4:
+                """Output"""
+                p1 = self.memory[param_pointer]
+                p1_mode = param_modes
+                operand1 = p1 if p1_mode else self.memory[p1]
+                outputs.append(operand1)
+                self.pointer += 2
+            elif opcode == 5:
+                """Jump if true"""
+                p1, p2 = self.memory[param_pointer : param_pointer + 2]
+                p2_mode, p1_mode = divmod(param_modes, 10)
+
+                operand1 = p1 if p1_mode else self.memory[p1]
+                operand2 = p2 if p2_mode else self.memory[p2]
+
+                if operand1 != 0:
+                    # Jump
+                    self.pointer = operand2
+                else:
+                    # No jump, regular increment
+                    self.pointer += 3
+            elif opcode == 6:
+                """Jump if false"""
+                p1, p2 = self.memory[param_pointer : param_pointer + 2]
+                p2_mode, p1_mode = divmod(param_modes, 10)
+
+                operand1 = p1 if p1_mode else self.memory[p1]
+                operand2 = p2 if p2_mode else self.memory[p2]
+
+                if operand1 == 0:
+                    # Jump
+                    self.pointer = operand2
+                else:
+                    # No jump, regular increment
+                    self.pointer += 3
+            elif opcode == 7:
+                """Less than"""
+                p1, p2, p3 = self.memory[param_pointer : param_pointer + 3]
+
+                param_modes, p1_mode = divmod(param_modes, 10)
+                p3_mode, p2_mode = divmod(param_modes, 10)
+
+                operand1 = p1 if p1_mode else self.memory[p1]
+                operand2 = p2 if p2_mode else self.memory[p2]
+
+                self.memory[p3] = operand1 < operand2
+
+                self.pointer += 4
+            elif opcode == 8:
+                """Equals"""
+                p1, p2, p3 = self.memory[param_pointer : param_pointer + 3]
+
+                param_modes, p1_mode = divmod(param_modes, 10)
+                p3_mode, p2_mode = divmod(param_modes, 10)
+
+                operand1 = p1 if p1_mode else self.memory[p1]
+                operand2 = p2 if p2_mode else self.memory[p2]
+
+                self.memory[p3] = operand1 == operand2
 
                 self.pointer += 4
 
--- tests/test_2019_intcode.py
+++ tests/test_2019_intcode.py
@@ -1,17 +1,159 @@
+import random
+
 from aoc.aoc2019.intcode import Intcode
 
 
-def test_opcode1():
-    ic = Intcode([1, 0, 0, 0, 99])
+def test_add():
+    program = list(map(int, "1,0,0,0,99".split(",")))
+    ic = Intcode(program)
     ic.run()
     assert ic.memory == [2, 0, 0, 0, 99]
 
 
-def test_opcode2():
-    ic = Intcode([2, 3, 0, 3, 99])
+def test_multiply():
+    program = list(map(int, "2,3,0,3,99".split(",")))
+    ic = Intcode(program)
     ic.run()
     assert ic.memory == [2, 3, 0, 6, 99]
 
-    ic = Intcode([2, 4, 4, 5, 99, 0])
+    program = list(map(int, "2,4,4,5,99,0".split(",")))
+    ic = Intcode(program)
     ic.run()
     assert ic.memory == [2, 4, 4, 5, 99, 9801]
+
+
+def test_opcode3_4():
+    """Outputs whatever it gets as input"""
+    program = list(map(int, "3,0,4,0,99".split(",")))
+    ic = Intcode(program)
+    random_val = random.randint(0, 1024)
+    output = ic.run(random_val)
+    assert len(output) == 1
+    assert output[0] == random_val
+
+
+def test_param_mode():
+    program = list(map(int, "1002,4,3,4,33".split(",")))
+    ic = Intcode(program)
+    ic.run()
+    assert ic.memory == [1002, 4, 3, 4, 99]
+
+
+def test_jump_position_mode():
+    """Take an input, output 0 if input was 0 or 1 if input was non-zero"""
+    program = list(map(int, "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9".split(",")))
+    ic = Intcode(program)
+    output = ic.run(0)
+    assert len(output) == 1
+    assert output[0] == 0
+
+    ic = Intcode(program)
+    output = ic.run(random.randint(1, 100))
+    assert len(output) == 1
+    assert output[0] == 1
+
+
+def test_jump_immediate_mode():
+    """Take an input, output 0 if input was 0 or 1 if input was non-zero"""
+    program = list(map(int, "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9".split(",")))
+    ic = Intcode(program)
+    output = ic.run(0)
+    assert len(output) == 1
+    assert output[0] == 0
+
+    ic = Intcode(program)
+    output = ic.run(random.randint(1, 100))
+    assert len(output) == 1
+    assert output[0] == 1
+
+
+def test_equals_position_mode():
+    """Using position mode, consider whether the input
+    is equal to 8; output 1 (if it is) or 0 (if it is not)."""
+    program = list(map(int, "3,9,8,9,10,9,4,9,99,-1,8".split(",")))
+
+    ic = Intcode(program)
+    output = ic.run(1)
+    assert len(output) == 1
+    assert output[0] == 0
+
+    ic = Intcode(program)
+    output = ic.run(8)
+    assert len(output) == 1
+    assert output[0] == 1
+
+
+def test_equals_immediate_mode():
+    """UUsing immediate mode, consider whether the input
+    is equal to 8; output 1 (if it is) or 0 (if it is not)."""
+    program = list(map(int, "3,3,1108,-1,8,3,4,3,99".split(",")))
+
+    ic = Intcode(program)
+    output = ic.run(1)
+    assert len(output) == 1
+    assert output[0] == 0
+
+    ic = Intcode(program)
+    output = ic.run(8)
+    assert len(output) == 1
+    assert output[0] == 1
+
+
+def test_less_than_position_mode():
+    """Using position mode, consider whether the input
+    is less than 8; output 1 (if it is) or 0 (if it is not)."""
+    program = list(map(int, "3,9,7,9,10,9,4,9,99,-1,8".split(",")))
+
+    ic = Intcode(program)
+    output = ic.run(1)
+    assert len(output) == 1
+    assert output[0] == 1
+
+    ic = Intcode(program)
+    output = ic.run(8)
+    assert len(output) == 1
+    assert output[0] == 0
+
+
+def test_less_than_immediate_mode():
+    """Using immediate mode, consider whether the input
+    is less than 8; output 1 (if it is) or 0 (if it is not)."""
+    program = list(map(int, "3,3,1107,-1,8,3,4,3,99".split(",")))
+
+    ic = Intcode(program)
+    output = ic.run(1)
+    assert len(output) == 1
+    assert output[0] == 1
+
+    ic = Intcode(program)
+    output = ic.run(8)
+    assert len(output) == 1
+    assert output[0] == 0
+
+
+def test_less_greater_equals_8():
+    """The example program uses an input instruction to ask for a single number.
+    The program will then output 999 if the input value is below 8,
+    output 1000 if the input value is equal to 8,
+    or output 1001 if the input value is greater than 8."""
+    program_str = (
+        "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,"
+        "1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,"
+        "999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
+    )
+    program = list(map(int, program_str.split(",")))
+
+    ic = Intcode(program)
+    output = ic.run(random.randint(-10, 7))
+    assert len(output) == 1
+    assert output[0] == 999
+
+    ic = Intcode(program)
+    output = ic.run(8)
+    assert len(output) == 1
+    assert output[0] == 1000
+
+    ic = Intcode(program)
+    output = ic.run(random.randint(9, 100))
+    assert len(output) == 1
+    assert output[0] == 1001
