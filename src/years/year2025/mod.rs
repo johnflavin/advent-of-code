364388c47099d7626c69e107184b1e52887f632a
John Flavin <flavinj@gmail.com>
2025-12-10 08:52:53 -0600

    Remove all 2025 days > 12

--- src/years/year2025/mod.rs
+++ src/years/year2025/mod.rs
@@ -12,19 +12,6 @@ pub mod day09;
 pub mod day10;
 pub mod day11;
 pub mod day12;
-pub mod day13;
-pub mod day14;
-pub mod day15;
-pub mod day16;
-pub mod day17;
-pub mod day18;
-pub mod day19;
-pub mod day20;
-pub mod day21;
-pub mod day22;
-pub mod day23;
-pub mod day24;
-pub mod day25;
 
 /// Get a solver for a specific day in 2025
 pub fn get_solver(day: u8) -> Option<Box<dyn Solver>> {
@@ -41,19 +28,6 @@ pub fn get_solver(day: u8) -> Option<Box<dyn Solver>> {
         10 => Some(Box::new(day10::Day10)),
         11 => Some(Box::new(day11::Day11)),
         12 => Some(Box::new(day12::Day12)),
-        13 => Some(Box::new(day13::Day13)),
-        14 => Some(Box::new(day14::Day14)),
-        15 => Some(Box::new(day15::Day15)),
-        16 => Some(Box::new(day16::Day16)),
-        17 => Some(Box::new(day17::Day17)),
-        18 => Some(Box::new(day18::Day18)),
-        19 => Some(Box::new(day19::Day19)),
-        20 => Some(Box::new(day20::Day20)),
-        21 => Some(Box::new(day21::Day21)),
-        22 => Some(Box::new(day22::Day22)),
-        23 => Some(Box::new(day23::Day23)),
-        24 => Some(Box::new(day24::Day24)),
-        25 => Some(Box::new(day25::Day25)),
         _ => None,
     }
 }
